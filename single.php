4853f5ca1797a0448db410f93dc472c30c360309
Tom Royer <royertom.fr@gmail.com>
2015-02-15 21:24:26 -0500

    add mobile size sc

--- single.php
+++ single.php
@@ -55,7 +55,8 @@
 	          <?php the_field('session_description'); ?>
           </p>
 			<p>
-	          <iframe width="500" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/<?php the_field('soundcloud_embed'); ?>?secret_token=<?php the_field('soundcloud_secret_token'); ?>&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
+				<?php if( wp_is_mobile() ) { $width_sc = 200; } else { $width_sc = 500; } ?>
+	          <iframe width="<?= $width_sc ?>" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/<?php the_field('soundcloud_embed'); ?>?secret_token=<?php the_field('soundcloud_secret_token'); ?>&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
           </p>
         </div>
 	      </div>
