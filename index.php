8d1b40b832047f980ae05a6d10c667beb9a552cd
Tom Royer <royertom.fr@gmail.com>
2015-03-25 09:26:50 -0400

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 		            </div>
 		            <div class="name">
 			            <span>
-				            <?php str_replace(' ',"\n",strtoupper(the_title())); ?>
+				            <?php str_replace(" ","<br \>",strtoupper(the_title())); ?>
 			            </span>
 		            </div>
 	            </h1>
